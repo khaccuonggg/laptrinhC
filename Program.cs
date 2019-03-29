344e07ceacf6e8e9a8bea58060ce4c7d3da913c0
khaccuonggg <48077462+khaccuonggg@users.noreply.github.com>
2019-03-29 11:18:58 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,58 +1,29 @@
 ﻿using System;
 
-namespace SinhVien
+namespace QuanLiSach
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int n;
-            Console.WriteLine("nhap so sinh vien:");
-            n = Int32.Parse(Console.ReadLine());
-            SinhVien[] sv = new SinhVien[n];
+            Console.WriteLine("Nhap vao so loai sach:");
+            n = int.Parse(Console.ReadLine());
+            SachGiaoKhoa[] sgk = new SachGiaoKhoa[n];
             for (int i = 0; i < n; i++)
             {
-                sv[i] = new SinhVien();
-                sv[i].nhap();
-                sv[i].IN();
-            }
-
-            float min = sv[0].tinhdiemtb();
-            for (int i = 1; i < n; i++)
-            {
-                if (sv[i].tinhdiemtb() < min)
-                    min = sv[i].tinhdiemtb();
+                Console.WriteLine("Nhap thong tin sach thu: " + (i + 1).ToString());
+                sgk[i] = new SachGiaoKhoa();
+                sgk[i].Nhap();
 
             }
-            Console.WriteLine("SV diem thap nhat la:{0}", min);
-            for (int j = 0; j < n; j++)
+            Console.WriteLine("Danh sach Sach:");
+            Console.WriteLine("{0,-15}{1,-15}{2,-15}{3,-15}", "maSach", "Soluong", "Dongia", "Thanhtien");
+            for (int i = 0; i <= n; i++)
             {
-                for (int k = j + 1; k < n; k++)
-                {
-                    if (sv[k].tinhdiemtb() < sv[j].tinhdiemtb())
-                    {
-                        SinhVien t = new SinhVien();
-                        t = sv[j];
-                        sv[j] = sv[k];
-                        sv[k] = t;
-                    }
-                }
+                sgk[i].Hienthi();
             }
-            Console.WriteLine(" Sap xep diem trung binh theo thu tu tang dan cua SV:\n");
-            for (int j = 0; j < n; j++)
-            {
-                Console.WriteLine("SV ma {0} co diem trung binh {1} xep thu nhat", sv[j].maSV, sv[j].tinhdiemtb());
-            }
-            for (int i = 0; i < n; i++)
-            {
-                if (sv[i].maSV.Contains("IT"))
-                    Console.WriteLine("sinh vien ma co chua ki tu IT la {0}", sv[i].maSV);
-            }
-
             Console.ReadLine();
         }
     }
 }
-
-
